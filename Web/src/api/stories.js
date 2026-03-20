7da0a7f93ea44cc6454d7fb7083ff88184f06073
Dainius Kokolovas <dainius092025@gmail.com>
2026-03-20 10:19:28 +0100

    changed port in stoies.js

--- Web/src/api/stories.js
+++ Web/src/api/stories.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5041";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5162";
 
 // SearchMode enum values from your API.
 export const SearchMode = {
